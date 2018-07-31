87515606cfa96dd8894adf62d32fb13612f56d22
JackyWu47 <jackyzero2000@hotmail.com>
2018-07-31 22:51:58 +0800

    HomeWork
    Sum 1 to 100

---
+++ HomeWorkTest/HomeWorkTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HomeWorkTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i,sum=0;
+            for(i=0;i<=100;i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
